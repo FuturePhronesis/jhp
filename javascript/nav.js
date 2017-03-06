cd3c1d0f8bbd444c0f5fdc49b5cd2709c6294487
Thomas Moore <tmoore712@gmail.com>
2017-03-05 23:50:16 -0500

    trying to figure out top nav responsive.

---
+++ javascript/nav.js
@@ -0,0 +1,11 @@
+/**
+ * Created by thomasmoore on 3/5/17.
+ */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
