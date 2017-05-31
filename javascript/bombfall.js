a1cbfbde3b8b995f88a4bf35e72c1d94053ee392
Thomas Moore <tmoore712@gmail.com>
2017-05-30 23:06:19 -0400

    blockquotes for timeline

---
+++ javascript/bombfall.js
@@ -0,0 +1,25 @@
+/**
+ * Created by thomasmoore on 5/29/17.
+ */
+var windw = this;
+
+$.fn.followTo = function ( pos ) {
+    var $this = this,
+        $window = $(windw);
+
+    $window.scroll(function(e){
+        if ($window.scrollTop() > pos) {
+            $this.css({
+                position: 'absolute',
+                top: pos
+            });
+        } else {
+            $this.css({
+                position: 'fixed',
+                top: 0
+            });
+        }
+    });
+};
+
+$('.sidebar-bomb').followTo(250);
